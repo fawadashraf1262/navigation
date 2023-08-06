842de4ce6a40ae40329ea49b2fcc12c8c8672ac6
fawadashraf1262 <fawadashraf1262@gmail.com>
2023-08-06 09:20:22 +0500

    navigation completed with hooks

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,46 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { House, Scroll, ImageSquare, AngularLogo, User } from 'phosphor-react'
-import Headers from './Headers'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { House, Scroll, ImageSquare, AngularLogo, User } from 'phosphor-react';
 
 const Header = () => {
+  const [activeTab, setActiveTab] = useState('Home');
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   return (
     <div className='navigation'>
-        <ul>
-            <li className='list active'>
-                <Link to='/'>
-                    <span className='icon'><House size={32} weight="thin" /></span>
-                    <span className='text'>Home</span>
-                </Link>
-            </li>
-            <li className='list'>
-                <Link to='/blog'>
-                    <span className='icon'><Scroll size={32} weight="thin" /></span>
-                    <span className='text'>Blog</span>
-                </Link>
-            </li>
-            <li className='list'>
-                <Link to='/gallery'>
-                    <span className='icon'><ImageSquare size={32} weight="thin" /></span>
-                    <span className='text'>Gallery</span>
-                </Link>
-            </li>
-            <li className='list'>
-                <Link to='/about'>
-                    <span className='icon'><AngularLogo size={32} weight="thin" /></span>
-                    <span className='text'>About</span>
-                </Link>
-            </li>
-            <li className='list'>
-                <Link to='/contact'>
-                    <span className='icon'><User size={32} weight="thin" /></span>
-                    <span className='text'>Contact</span>
-                </Link>
-            </li>
-            <div className="indicator"></div>
-        </ul>
+      <ul>
+        <li className={`list ${activeTab === 'Home' ? 'active' : ''}`}>
+          <Link to='/' onClick={() => handleTabClick('Home')}>
+            <span className='icon'><House size={32} weight="thin" /></span>
+            <span className='text'>Home</span>
+          </Link>
+        </li>
+        <li className={`list ${activeTab === 'Blog' ? 'active' : ''}`}>
+          <Link to='/blog' onClick={() => handleTabClick('Blog')}>
+            <span className='icon'><Scroll size={32} weight="thin" /></span>
+            <span className='text'>Blog</span>
+          </Link>
+        </li>
+        <li className={`list ${activeTab === 'Gallery' ? 'active' : ''}`}>
+          <Link to='/gallery' onClick={() => handleTabClick('Gallery')}>
+            <span className='icon'><ImageSquare size={32} weight="thin" /></span>
+            <span className='text'>Gallery</span>
+          </Link>
+        </li>
+        <li className={`list ${activeTab === 'About' ? 'active' : ''}`}>
+          <Link to='/about' onClick={() => handleTabClick('About')}>
+            <span className='icon'><AngularLogo size={32} weight="thin" /></span>
+            <span className='text'>About</span>
+          </Link>
+        </li>
+        <li className={`list ${activeTab === 'Contact' ? 'active' : ''}`}>
+          <Link to='/contact' onClick={() => handleTabClick('Contact')}>
+            <span className='icon'><User size={32} weight="thin" /></span>
+            <span className='text'>Contact</span>
+          </Link>
+        </li>
+        <div className={`indicator ${activeTab === 'Home' ? 'home' : ''} ${activeTab === 'Blog' ? 'blog' : ''} ${activeTab === 'Gallery' ? 'gallery' : ''} ${activeTab === 'About' ? 'about' : ''} ${activeTab === 'Contact' ? 'contact' : ''}`}></div>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div  style={{textAlign: "center",color: "white"}}>
-        <h1>Home</h1>
-        <Link style={{textDecoration:"none", color: "white", backgroundColor: "blue", padding: "10px", borderRadius: "14px"}} to="/">Go Back</Link>
+      <h1>Hello world</h1>    
     </div>
   )
 }
