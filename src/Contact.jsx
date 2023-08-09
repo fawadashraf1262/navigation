328220044dbc2f8aa98067a0f3a5ba5b1cfcd910
fawadashraf1262 <fawadashraf1262@gmail.com>
2023-08-09 20:45:58 +0500

    testing global states

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Link } from 'react-router-dom'
+import '../src/index.css'
 const Contact = () => {
+  const [activeTab, setActiveTab] = useState('Home')
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   return (
     <div  style={{textAlign: "center",color: "white"}}>
-        <h1>Contact</h1>
-        <Link style={{textDecoration:"none", color: "white", backgroundColor: "blue", padding: "10px", borderRadius: "14px"}} to="/">Go Back</Link>
+        <h1>Coact</h1>
+        <Link style={{textDecoration:"none", color: "white", backgroundColor: "blue", padding: "10px", borderRadius: "14px"}} to="/" onClick={() => handleTabClick('Home') } className={`${activeTab === 'Home'}`}>Go Back</Link>
     </div>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/Home.css'
 import { Link } from 'react-router-dom'
 const Home = () => {
+  
   return (
     <div className='App'  style={{textAlign: "center"}}>
       <h1>Hello world</h1>    
